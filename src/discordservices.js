5253635f0c13f6cf7bb8e47c3a4c4d7948b0b84b
Nerduhm <58738637+willuhm-js@users.noreply.github.com>
2020-05-02 13:38:43 -0400

    Update discordservices.js

--- src/discordservices.js
+++ src/discordservices.js
@@ -2,19 +2,20 @@ const https = require('https')
 let globalToken = ''
 
 exports.login = function(token) {
-  if (!token) return console.error('dservice error: no token provided')
-  if (''+typeof(token) != 'string') return console.error('dservice error: token must be a string')
+  if (!token) return console.error('discordservices.js error: no token provided')
+  if (''+typeof(token) != 'string') return console.error('discordservices.js error: token must be a string')
   globalToken = token;
 }
 
 exports.updateStats = function(id, size, shards) {
-  if (!id) return console.error('dservice error: id param is missing')
-  if (''+typeof(id) != 'string') return console.error('dservice error: bot id provided incorrectly (must be type string, it is type: '+typeof(id)+')')
-  if (''+typeof(size) != 'number' && size != undefined) return console.error('dservice error: guild size provided incorrectly (must be type number, it is type: '+typeof(size)+')')
-  if (''+typeof(shards) != 'number' && shards != undefined) return console.error('dservice error: shard size provided incorrectly (must be type number, it is type: '+typeof(shards)+')')
-  if (globalToken == '' || globalToken == undefined) return console.error('dservice error: not logged in')
+  if (!id) return console.error('discordservices.js error: id param is missing')
+  if (''+typeof(id) != 'string') return console.error('discordservices.js error: bot id provided incorrectly (must be type string, it is type: '+typeof(id)+')')
+  if (''+typeof(size) != 'number' && size != undefined) return console.error('discordservices.js error: guild size provided incorrectly (must be type number, it is type: '+typeof(size)+')')
+  if (''+typeof(shards) != 'number' && shards != undefined) return console.error('discordservices.js error: shard size provided incorrectly (must be type number, it is type: '+typeof(shards)+')')
+  if (globalToken == '' || globalToken == undefined) return console.error('discordservices.js error: not logged in')
   //so we have correct id and correct size, and correct token.
-  if (size == undefined && shards == undefined) return console.error('dservice error: both size and shards are not specified')
+  //if (size < 0) return console.error('discordservices.js error: cannot set size to a negative value!')
+  if (size == undefined && shards == undefined) return console.error('discordservices.js error: both size and shards are not specified')
   if (size == undefined) size = 0;
   if (shards == undefined) shards = 0
   const data = JSON.stringify({servers: size, shards: shards})
@@ -33,7 +34,7 @@ exports.updateStats = function(id, size, shards) {
   const req = https.request(options, res => {
     //console.log('status code: '+res.statusCode)
     res.on('data', d => {
-    return console.error('dservice error: '+d);
+    return console.error('discordservices.js error: '+d);
 
   })
   })
@@ -42,6 +43,71 @@ req.write(data)
 req.end()
 }
 
-exports.updateNews = function(id, title, desc) {
- /* wip */ 
+exports.postNews = function(id, title, content) {
+   if (!id) return console.error('discordservices.js error: id param is missing')
+  if (''+typeof(id) != 'string') return console.error('discordservices.js error: bot id provided incorrectly (must be type string, it is type: '+typeof(id)+')')
+  if (''+typeof(title) != 'string') return console.error('discordservices.js error: title text provided incorrectly (must be type string, it is type: '+typeof(title)+')')
+  if (''+typeof(content) != 'string') return console.error('discordservices.js error: content size provided incorrectly (must be type string, it is type: '+typeof(content)+')')
+  if (globalToken == '' || globalToken == undefined) return console.error('discordservices.js error: not logged in')
+  //so we have correct id and correct size, and correct token.
+  //if (size < 0) return console.error('discordservices.js error: cannot set size to a negative value!')
+  
+  const data = JSON.stringify({title: title, content: content, error: false})
+  const options = {
+  hostname: 'api.discordservices.net',
+  port: 443,
+  path: `/bot/${id}/news`,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+    'Authorization': globalToken
+  }
+  }
+
+  const req = https.request(options, res => {
+    //console.log('status code: '+res.statusCode)
+    res.on('data', d => {
+    return console.error('discordservices.js error: '+d);
+
+  })
+  })
+
+req.write(data)
+req.end()
+}
+
+exports.updateCommands = function(id, commands) {
+   if (globalToken == '' || globalToken == undefined) return console.error('discordservices.js error: not logged in')
+   if (!id) return console.error('discordservices.js error: id param is missing')
+  if (''+typeof(id) != 'string') return console.error('discordservices.js error: bot id provided incorrectly (must be type string, it is type: '+typeof(id)+')')
+  if (''+typeof(commands) != 'object') return console.error('discordservices.js error: commands object provided incorrectly (must be type object, it is type: '+typeof(title)+')')
+
+ 
+  //so we have correct id and correct size, and correct token.
+  //if (size < 0) return console.error('discordservices.js error: cannot set size to a negative value!')
+  
+  const data = JSON.stringify(commands)
+  const options = {
+  hostname: 'api.discordservices.net',
+  port: 443,
+  path: `/bot/${id}/commands`,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+    'Authorization': globalToken
+  }
+  }
+
+  const req = https.request(options, res => {
+    //console.log('status code: '+res.statusCode)
+    res.on('data', d => {
+    return console.error('discordservices.js error: '+d);
+
+  })
+  })
+
+req.write(data)
+req.end()
 }
